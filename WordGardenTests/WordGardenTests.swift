dd402446b2c7b9908c86750788da2dd78015ea59
aloluvsu <preciadoalondra758@gmail.com>
2026-01-13 11:43:05 -0800

    Initial Commit

---
+++ WordGardenTests/WordGardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenTests.swift
+//  WordGardenTests
+//
+//  Created by PRECIADO, ALONDRA on 1/13/26.
+//
+
+import Testing
+@testable import WordGarden
+
+struct WordGardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
