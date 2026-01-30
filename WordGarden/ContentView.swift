f8a12f4d1cfb1576c8c5c30fd5c073f5e6caf2ca
aloluvsu <preciadoalondra758@gmail.com>
2026-01-30 13:00:05 -0800

    Animation and Sound

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+   private static let  maximumGuesses = 8 // Need to refer to this as Self.maximumGuesses
+    
     @State private var wordsGuessed = 0
     @State private var wordsMissed = 0
     @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
@@ -15,11 +18,15 @@ struct ContentView: View {
     @State private var wordToGuess = ""
     @State private var revealedWord = ""
     @State private var lettersGuessed = ""
+    @State private var guessesRemaining = maximumGuesses
     @State private var guessedLetter = ""
     @State private var imageName = "flower8"
     @State private var  playAgainHiden = true
+    @State private var playAgainButtonLabel = "Another Word?"
+    @State private var audioPlayer: AVAudioPlayer!
     @FocusState private var textFieldIsFocused: Bool
     private let wordsToGuess = ["SWIFT", "DOG", "CAT"] // ALL Caps
+    
     
     
     var body: some View {
@@ -44,6 +51,8 @@ struct ContentView: View {
             Text(gameStatusMessage)
                 .font(.title)
                 .multilineTextAlignment(.center)
+                .frame(height: 80)
+                .minimumScaleFactor(0.5)
                 .padding()
             
             //TODO: Switch to wordsToGuess[currentWordIndex]
@@ -78,19 +87,35 @@ struct ContentView: View {
                                 return
                             }
                             guessALetter()
+                            updateGamePlay()
                         }
                     
                     Button("Guess a Letter:") {
                         guessALetter()
+                        updateGamePlay()
                     }
                     .buttonStyle(.bordered)
                     .tint(.mint)
                     .disabled(guessedLetter.isEmpty)
                 }
             } else {
-                Button("Another Word?") {
-                    //TODO: Another Word Button Action Here
-                    
+                Button(playAgainButtonLabel) {
+                    // If all the words have been guessed ...
+                    if currentWordIndex == wordsToGuess.count {
+                        currentWordIndex = 0
+                        wordsGuessed = 0
+                        wordsMissed = 0
+                        playAgainButtonLabel = "Another Word?"
+                    }
+                   
+                    //Reset after a word was guessed or missed
+                    wordToGuess = wordsToGuess[currentWordIndex]
+                    revealedWord = "_" + String(repeating: " _", count: wordToGuess.count-1)
+                    lettersGuessed = ""
+                    guessesRemaining = Self.maximumGuesses // because maximumGuesses is static
+                    imageName = "flower\(guessesRemaining)"
+                    gameStatusMessage = "How many Guesses to Uncover the Hidden Word? "
+                    playAgainHiden = true
                 }
                 .buttonStyle(.borderedProminent)
                 .tint(.mint)
@@ -102,6 +127,7 @@ struct ContentView: View {
             Image(imageName)
                 .resizable()
                 .scaledToFit()
+                .animation(.easeIn(duration:0.75), value: imageName)
         }
         .ignoresSafeArea(edges: .bottom)
         .onAppear {
@@ -116,8 +142,66 @@ struct ContentView: View {
          revealedWord = wordToGuess.map{ letter in
              lettersGuessed.contains(letter) ? "\(letter)" : "_"
              }.joined(separator: " ")
+        
+    }
+    func updateGamePlay() {
+        
+        if !wordToGuess.contains(guessedLetter) {
+            guessesRemaining -= 1
+            // Animate crumbling leaf and play the incorrect sound
+            imageName = "wilt\(guessesRemaining)"
+            playSound(soundName: "incorrect")
+            // Delay change to flower image until after wilt animation is done
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                imageName = "flower\(guessesRemaining)"
+            }
+        } else {
+            playSound(soundName: "correct")
+        }
+            
+        // When Do We Play Another Word?
+        if !revealedWord.contains("_") { // Guessed when no "_" in revealedWord
+            gameStatusMessage = " You Guessed It! It Took You \(lettersGuessed.count) Guesses to Guess the word."
+            wordsGuessed += 1
+            currentWordIndex += 1
+            playAgainHiden = false
+            playSound(soundName: "word-guessed")
+        } else if guessesRemaining ==  0 { // Word missed
+            gameStatusMessage = "So sorry, You're All Out of Guesses"
+            wordsMissed += 1
+            currentWordIndex += 1
+            playAgainHiden = false
+            playSound(soundName: "word-not-guessed")
+           
+        } else { // Keep Guessing
+            //TODO: Redo this with LocalizesStingKey & Inflect
+            gameStatusMessage = "You've Made \(lettersGuessed.count) Guess\(lettersGuessed.count == 1 ? "" : "es" )"
+        }
+       
+        if currentWordIndex == wordsToGuess.count {
+            playAgainButtonLabel = "Restart Game?"
+            gameStatusMessage = gameStatusMessage + "\nYou've Tried All of the Words. Restart from the Beggining?"
+        }
+        
         guessedLetter = ""
     }
+    
+    func playSound(soundName: String) {
+        if audioPlayer != nil &&  audioPlayer.isPlaying{
+           audioPlayer.stop()
+            
+        }
+    guard let soundFile = NSDataAsset(name: soundName) else {
+        print("😡 Could not read file named \(soundName)")
+        return
+    }
+    do {
+        audioPlayer =  try AVAudioPlayer(data: soundFile.data)
+        audioPlayer.play()
+    } catch {
+        print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+    }
+}
 }
 
 #Preview {
